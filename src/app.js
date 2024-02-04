42d30093129b1b8f8b96e58b6163797e0d361a68
Devi-CodingTime <devipurnima26899@gmail.com>
2024-02-04 22:49:25 +0530

    underline done

--- src/app.js
+++ src/app.js
@@ -10,5 +10,31 @@ app.use(express.json());
 
 // Write GET endpoint for sending product to the client here
 // Endpoint - /api/v1/products/:name/:price
+// app.use('//api/v1/products/:name/:price',(req,res)=>{
+//     console.log(req.params.name);
+//     console.log(req.params.price);
+
+//     const availableProduct = products.find(val => req.params.name == val.name)
+//     if(!availableProduct){
+//         return res.status(400).send({
+//             status: 'failure',
+//             message: 'Product not found!'
+//         })
+//     }
+//     return res.status(200).send({
+//         status: 'success',
+//         message: 'Product fetched successfully',
+//         data:{'product':availableProduct}
+//     })
+// })
+app.get('/api/v1/products', (req,res) => {
+    res.status(200).json({
+    status:'Success',
+    message:'Details of products fetched successfully',
+    data:{
+        products
+    }
+});
+});
 
 module.exports = app;
